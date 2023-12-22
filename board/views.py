81fe96a2b7f8a3f4971e83f62061e370df9825a0
yemmi <tlroal0305@naver.com>
2023-12-22 19:23:16 +0900

    게시판 리스트 완

--- board/views.py
+++ board/views.py
@@ -6,13 +6,15 @@ def board(request):
   # 게시글 리스트 
   if request.method == "GET":
     page = request.GET.get('page', 1)
-    post_set = Post.objects.all().order_by('-id')
+    search_text = request.GET.get('search_text', "")
+    post_set = Post.objects.filter(title__icontains = search_text).order_by('-id')
     paginator = Paginator(post_set, 4)
     
     post_set= paginator.get_page(page)
     
     context = {
-      "post_set": post_set
+      "post_set": post_set,
+      "search_text": search_text 
     }
     return render (request, 'page/index.html', context=context)
   
